d26201a417c62ff539ad6ed4bfba603cf04cc618
Retr024 <113691445+Retr024@users.noreply.github.com>
2022-10-01 16:40:57 +0200

    Update calcoloareaeperimetrodiuntriangolo.cpp

--- calcoloareaeperimetrodiuntriangolo.cpp
+++ calcoloareaeperimetrodiuntriangolo.cpp
@@ -10,25 +10,26 @@ calcolare l'area del triangolo eil suo perimetro conosciendo base e altezza
 #include "stdio.h" //printf, scanf
 int main()
 { //dichiarare le variabii
-	float base,altezza; //input
+	float base;
+	float altezza; //input
 	float area; //output
-	float lato; //input
+	float latoobliquo; //input
 	float perimetro; //output
 	
     //avvalorare input chiedendolo all'utente
-	printf("inserisci quanto la base\t");
-	scanf("%f",&base);
+	printf("inserisci quanto vale il lato obliquo\t");
+	scanf("%f",&latoobliquo);
 	printf("inserisci quanto vale l'altezza\t");
 	scanf("%f",&altezza);
-	printf("inserisci quanto vale il lato\t");
-	scanf("%f",&lato);
+	printf("inserisci quanto vale la base\t");
+	scanf("%f",&base);
 	
 	//elaborazione
-	area=base*altezza/2;
-	perimetro=lato+lato+lato;
+	area=(latoobliquo*altezza)/2;
+	perimetro=latoobliquo+latoobliquo+base;
 	
 	//visualizza output
-	printf("il triangolo con base %.2f e altezza %.2f la sua area e' %.2f'", base, altezza, area);
+	printf("il triangolo con i lati obliqui di %.2f e l'altezza di %.2f quindi la sua area e' %.2f'", latoobliquo, altezza, area);
 	printf(" e il perimetro del triangolo e' %.2f\t", perimetro);
 	
 	return 0;
