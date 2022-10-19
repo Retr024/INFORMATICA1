73ba0edaa7190a79eb2ca79147a4066fceb8620d
Retr024 <113691445+Retr024@users.noreply.github.com>
2022-10-19 14:08:23 +0200

    Add files via upload

---
+++ esercizio_6_del_libro.cpp
@@ -0,0 +1,38 @@
+/*
+file: due numeri interi
+autore: Simone Palacino
+classe: 3h
+data: 05/10/2022
+
+input:due numeri interi
+elaborazione: resto divisione tra due numeri interi
+output: resto
+*/
+
+#include <stdio.h> //printf, scanf
+int main()
+{
+	//dichiarare le variabili input
+	int a,b;
+	//e output
+	int c;
+	
+	printf("simone palacino \n");
+	printf ("inserisci un valore ad a \t");
+	scanf ("%d,",&a);
+	printf("inserisci un valore ad b \t");
+	scanf("%d",&b);
+	
+	if((a/b)>2*b))
+	{
+		c=a-b;
+	}
+	else
+	{
+		c=b-a;
+	}
+	 
+	printf("il riaultato e' %d",c);
+	
+	return 0;
+}
