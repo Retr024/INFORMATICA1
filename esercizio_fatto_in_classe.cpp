4b862961a1e3a7a95d6ffd2a7b2ade7f67e7774b
Retr024 <113691445+Retr024@users.noreply.github.com>
2022-10-21 12:45:28 +0200

    Add files via upload

---
+++ esercizio_fatto_in_classe.cpp
@@ -0,0 +1,36 @@
+/*
+file: due numeri interi
+autore: Simone Palacino
+classe: 3h
+data: 05/10/2022
+
+input:due numeri interi
+elaborazione: resto divisione tra due numeri interi
+output: resto
+*/
+
+#include <stdio.h> //printf, scanf
+int main()
+{
+	//dichiarare le variabili input
+	int a,b;
+	//e output
+	int c;
+	
+	printf("simone palacino \n");
+	printf ("inserisci un numero intero \t");
+	scanf ("%d,",&a);
+	printf("inserisci un numero intero \t");
+	scanf("%d",&b);
+	
+	if(a<b)
+	{
+		printf(" %d e' minore di %d",a,b);
+	}
+	else
+	{
+		printf("%d e' minore di %.d",b,a);
+	}
+	
+	return 0;
+}
