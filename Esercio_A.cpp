ea028e846f8645306949ba7cde105b01725931af
Retr024 <113691445+Retr024@users.noreply.github.com>
2022-11-30 14:02:03 +0100

    Add files via upload

---
+++ Esercio_A.cpp
@@ -0,0 +1,14 @@
+/*
+file: due numeri interi
+autore: Simone Palacino
+classe: 3h
+data: 05/10/2022
+input:due numeri interi
+elaborazione: resto divisione tra due numeri interi
+output: resto
+*/
+
+#include <stdio.h> //printf, scanf
+int main()
+{
+	//dichiarare le variabili input
