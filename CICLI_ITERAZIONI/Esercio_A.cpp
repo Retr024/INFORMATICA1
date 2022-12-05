c1f2909e85bce27658db477ad5cee8de2bfbd6d8
Retr024 <113691445+Retr024@users.noreply.github.com>
2022-12-05 08:55:16 +0100

    Update Esercio_A.cpp

--- CICLI_ITERAZIONI/Esercio_A.cpp
+++ CICLI_ITERAZIONI/Esercio_A.cpp
@@ -12,3 +12,17 @@ output: resto
 int main()
 {
 	//dichiarare le variabili input
+	int num1;
+	int divisore;
+	
+	printf("inserisci un numero intero\t");
+	scanf("%d",&num1);
+	
+	for(divisore=2;divisore<2;divisore++)
+	{
+		if(num1%divisore==0)
+			printf("il divisore e %d\t",divisore);
+	}
+		printf("il divisore e %d",divisore);
+	return 0;	
+}
