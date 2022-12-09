34887aadf312247bbb62314f66c7d77e348f121c
Retr024 <113691445+Retr024@users.noreply.github.com>
2022-12-09 15:53:12 +0100

    Update Esercizio_D.cpp

--- CICLI_ITERAZIONI/Esercizio_D.cpp
+++ CICLI_ITERAZIONI/Esercizio_D.cpp
@@ -14,17 +14,17 @@ int main()
 	int num;
 	int cont;
 	int numprim;
-	
-	for(num=1;num!=100;num++)
+	//ciclo for inizio
+	for(num=1;num!=100;num++)//diamo un valore ad 1 e una condizione a num
 	{
-		numprim=0;
+		numprim=0;//diamo un valore a numprim
 		
-		for(cont=2;cont<num;cont++)
+		for(cont=2;cont<num;cont++)//diamo un valore a cont e una condizione sempre a cont
 		{
-			if(num%cont==0)
+			if(num%cont==0)//visualizziamo il resto con la % che equivale a 0
 				numprim=1;		
 		}
-		if(numprim==0)
+		if(numprim==0)//diciamo a numprim di essere uguale a 0
 			printf("%d e un numero primo\n",num);
 	}
 	return 0;
