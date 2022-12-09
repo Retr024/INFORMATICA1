e00dd835b8014984df5089d7364f6b090fdc3046
Retr024 <113691445+Retr024@users.noreply.github.com>
2022-12-09 15:57:12 +0100

    Update Esercizio_F.cpp

--- CICLI_ITERAZIONI/Esercizio_F.cpp
+++ CICLI_ITERAZIONI/Esercizio_F.cpp
@@ -17,17 +17,17 @@ int main()
 	int molt=7;
 	
 	printf ("inserisci 8 bit uno alla volta\n");
-	
+	//inizio ciclo con do e while con scanf al suo interno
 	do
 	{
 		scanf("%d",&num1);
-		risultato=risultato+pow(2,molt)*num1;
-		molt--;
+		risultato=risultato+pow(2,molt)*num1;//formula per trasformare un numero da*
+		molt--;                              //*8bit in decimale
 	}
 	
 	while(molt>-1);
 	
-		printf("il numero in decimale e\t %d",risultato);
-	
+		printf("il numero in decimale e\t %d",risultato);//printf finale
+	//fine programma
 	return 0;
 }
